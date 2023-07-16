573c1704fb0326a09eecccf0a20be0e78430de8d
Otmeal <songoatmeal@gmail.com>
2023-07-16 19:48:55 +0800

    Updated scoreboard

--- archery_game_scoring/src/features/Screen/SubGamesBar.tsx
+++ archery_game_scoring/src/features/Screen/SubGamesBar.tsx
@@ -1,13 +1,6 @@
-import SubGameTag from "./SubGameTag";
 import GroupList from "./GroupList/GroupList";
 import PhaseList from "./PhaseList/PhaseList";
 
-let subGameTitles = [
-    '新生',
-    '大專30m',
-    '公開30m',
-    '公開70m',
-]
 
 
 function SubGamesBar() {
