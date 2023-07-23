8f2caa8980cd9f3f51c8ad573ab368381d2444dd
b09902138 <b09902138@ntu.edu.tw>
2023-07-23 14:52:38 +0800

    simple login/logout \ login without any check, adding cookie

---
+++ backend/apis/user.go
@@ -0,0 +1,79 @@
+package apis
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"fmt"
+)
+
+func Login(c *gin.Context) {
+	// Perform login authentication
+	fmt.Println("Login here")
+	username := c.PostForm("username")
+	password := c.PostForm("password")
+	if Authenticate(username, password) {
+		// Set authentication cookie
+		cookie := http.Cookie{
+			Name:     "auth",
+			Value:    username,
+			HttpOnly: true,
+		}
+		http.SetCookie(c.Writer, &cookie)
+
+		c.JSON(http.StatusOK, gin.H{"result": "success"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"result": "fail"})
+}
+
+func Logout(c *gin.Context) {
+	// Clear authentication cookie
+	cookie := http.Cookie{
+		Name:     "auth",
+		Value:    "",
+		MaxAge:   -1,
+		HttpOnly: true,
+	}
+	http.SetCookie(c.Writer, &cookie)
+}
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		fmt.Println("authmiddleware here")
+		if !IsAuthenticated(c) {
+			c.Abort()
+			
+			fmt.Println("auth fail")
+			return
+		}
+
+		// If authenticated, continue to the next handler
+		fmt.Println("auth success")
+		c.Next()
+	}
+}
+
+// IsAuthenticated checks if the user is authenticated
+func IsAuthenticated(c *gin.Context) bool {
+	// Check if the authentication cookie exists
+	cookie, err := c.Cookie("auth")
+	if err != nil || cookie == "" {
+		return false
+	}
+
+	// Validate the authentication cookie (e.g., check against session store, database, etc.)
+	// ...
+
+	// Return true if authenticated, false otherwise
+	return true
+}
+
+// Authenticate performs the login authentication
+func Authenticate(username, password string) bool {
+	// Validate the username and password (e.g., check against database, external API, etc.)
+	// ...
+
+	// Return true if authentication is successful, false otherwise
+	return true
+}
---
+++ backend/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"backend/apis"
+)
+
+func main() {
+	router := routerSetup()
+	
+	router.Run("127.0.0.1:8080")
+}
+
+func routerSetup() *gin.Engine {
+	router := gin.Default()
+
+	sr := router.Group("/api")
+	{
+		sr.GET("/", apis.AuthMiddleware(), func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"Status": "ok"});
+		})
+
+		sr.POST("/login", apis.Login)
+
+		sr.GET("/logout", apis.Logout)
+	}
+
+	return router
+}
+
