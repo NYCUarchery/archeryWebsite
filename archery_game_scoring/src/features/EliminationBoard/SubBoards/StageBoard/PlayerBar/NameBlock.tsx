2cb35c79d80353f9899940ae8daa2d4643514d0b
Otmeal <songoatmeal@gmail.com>
2023-07-26 11:08:25 +0800

    refector: refector elimination board
    This added special color for winner in a match,
    and changed score bar appearance.

--- archery_game_scoring/src/features/EliminationBoard/SubBoards/StageBoard/PlayerBar/NameBlock.tsx
+++ archery_game_scoring/src/features/EliminationBoard/SubBoards/StageBoard/PlayerBar/NameBlock.tsx
@@ -1,12 +1,7 @@
 interface Props {
   name: string;
-  isWinner: boolean;
 }
 
 export default function NameBlock(props: Props) {
-  let className: string;
-  props.isWinner
-    ? (className = "name_block winner")
-    : (className = "name_block");
-  return <div className={className}>{props.name}</div>;
+  return <div className="name_block">{props.name}</div>;
 }
