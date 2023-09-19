21e5f117ea3c44087f1842a036ecf563525ebf7e
Otmeal <songoatmeal@gmail.com>
2023-09-19 10:16:32 +0800

    fix: fix the bug of the group phase tag
    This fix the bug of the GroupPhaseTag.tsx, which causes the page can't be loaded.
    FackLaneInfo.json is a unecessary file, so this deleted it.

--- archery_game_scoring/src/features/Screen/SubGameBar/GroupPhaseTag.tsx
+++ archery_game_scoring/src/features/Screen/SubGameBar/GroupPhaseTag.tsx
@@ -5,6 +5,11 @@ export default function GroupPhaseTag() {
   const userGroup = useSelector((state: any) => state.user.userGroup);
   const groups = useSelector((state: any) => state.game.groups);
   const phases = useSelector((state: any) => state.game.phases);
+
+  if (groups === undefined || phases === undefined) {
+    return <></>;
+  }
+
   return (
     <div className="group_phase_tag">
       {groups[userGroup]} {phases[currentPhase]}
