74c65849e873a1feed159cb0a45e570365b936d1
rain72510 <rain72510@gmail.com>
2023-09-23 16:48:22 +0800

    Add allow method: GET, POST

--- backend/main.go
+++ backend/main.go
@@ -51,7 +51,7 @@ func allowCORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
 		c.Header("Access-Control-Allow-Credentials", "true")
-		c.Header("Access-Control-Allow-Methods", "OPTIONS, PUT")
+		c.Header("Access-Control-Allow-Methods", "OPTIONS, PUT, GET, POST")
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204) // or 400 or..., need a HTTP StatusOK
 			return
