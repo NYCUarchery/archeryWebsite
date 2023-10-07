7f51af3a6c7f0c867dbe88d829dae65dff7efd74
Otmeal <songoatmeal@gmail.com>
2023-10-07 16:10:02 +0800

    feat: add the concept of "order" into the system.
    This add the concept of "order" into the system.
    The order is used to sort the players in the same lane.
    Order some time decides the shooting order.
    
    This also disable demo functionality of the GameStructureBoard.

--- archery_game_scoring/src/features/AdministrationBoard/GameStructureBoard/QualificationBoard/GroupBoard/GroupBoard.tsx
+++ archery_game_scoring/src/features/AdministrationBoard/GameStructureBoard/QualificationBoard/GroupBoard/GroupBoard.tsx
@@ -10,7 +10,8 @@ export default function GroupBoard() {
     (state: any) => state.gameStructureGroupMenu.groupShown
   );
   const group = useSelector(
-    (state: any) => state.groupsBoard.groups[groupShown]
+    (state: any) =>
+      state.gameStructureBoard.qualificationInfo.groups[groupShown]
   );
   const startLane = useSelector(
     (state: any) => state.game.qualification.groups[groupShown].start_lane
@@ -25,29 +26,14 @@ export default function GroupBoard() {
     dispatch(setGroups(players));
   }, []);
 
-  if (group.length === 0) return <div></div>;
-
   let lists = [];
-  let noLanePlayers = [];
-  let lanesplayers = create2DArray(endLane - startLane + 1, 0);
-  for (let i = 0; i < group.length; i++) {
-    if (
-      group[i].lane === 0 ||
-      group[i].lane - startLane >= endLane - startLane + 1 ||
-      group[i].lane - startLane < 0
-    ) {
-      noLanePlayers.push(group[i]);
-      continue;
-    }
-    lanesplayers[group[i].lane - startLane].push(group[i]);
-  }
 
   for (let i = startLane; i <= endLane; i++) {
     lists.push(
       <Grid key={i} item xs={4}>
         <LanePlayerList
           laneNum={i}
-          players={lanesplayers[i - startLane]}
+          players={group.lanes[i - startLane].players}
         ></LanePlayerList>
       </Grid>
     );
@@ -55,16 +41,8 @@ export default function GroupBoard() {
 
   return (
     <Box sx={{ width: "70%", display: "flex" }}>
-      <NoLanePlayerList players={noLanePlayers}></NoLanePlayerList>
+      <NoLanePlayerList players={group.no_lane_players}></NoLanePlayerList>
       <Grid container>{lists}</Grid>
     </Box>
   );
-}
-
-function create2DArray(rows: number, columns: number) {
-  var x = new Array(rows);
-  for (var i = 0; i < rows; i++) {
-    x[i] = new Array(columns);
-  }
-  return x;
 }
