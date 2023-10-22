377e1ead8d6dbaaa66d6aea5642807eefe2f8f64
Otmeal <songoatmeal@gmail.com>
2023-10-22 23:46:08 +0800

    feat: add player status block
    This shows if players confirmed their scores or not in the current end.

--- archery_game_scoring/src/features/AdministrationBoard/ProgressBoard/ProgressBoard.tsx
+++ archery_game_scoring/src/features/AdministrationBoard/ProgressBoard/ProgressBoard.tsx
@@ -2,15 +2,24 @@ import { Box } from "@mui/material";
 import PhaseSelector from "../PhaseSelector/PhaseSelector";
 import EndSwitch from "./EndSwitch/EndSwitch";
 import OverviewBlock from "./OverviewBlock/OverviewBlock";
+import PlayerStatusBlock from "./PlayerStatusBlock/PlayerStatusBlock";
 
 export default function ProgressBoard() {
   return (
-    <div className="process_board">
+    <>
       <PhaseSelector></PhaseSelector>
-      <Box sx={{ width: "300px", height: "100%" }}>
-        <EndSwitch></EndSwitch>
-        <OverviewBlock></OverviewBlock>
+      <Box
+        className="process_board"
+        sx={{
+          display: "flex",
+        }}
+      >
+        <Box sx={{ width: "300px", height: "100%" }}>
+          <EndSwitch></EndSwitch>
+          <OverviewBlock></OverviewBlock>
+        </Box>
+        <PlayerStatusBlock></PlayerStatusBlock>
       </Box>
-    </div>
+    </>
   );
 }
