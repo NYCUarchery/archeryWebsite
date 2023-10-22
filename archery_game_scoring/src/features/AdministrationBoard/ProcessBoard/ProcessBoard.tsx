731c5d1e46db53868b3a75a08ee791acb09e23c5
Otmeal <songoatmeal@gmail.com>
2023-10-22 20:53:28 +0800

    feat: add phase selector in admin admin board.
    This add a phase selector in the admin board.
    This controlls which phase the subboards of the admin board are showing.

--- archery_game_scoring/src/features/AdministrationBoard/ProcessBoard/ProcessBoard.tsx
+++ archery_game_scoring/src/features/AdministrationBoard/ProcessBoard/ProcessBoard.tsx
@@ -1,3 +1,9 @@
+import PhaseSelector from "../PhaseSelector/PhaseSelector";
+
 export default function ProcessBoard() {
-  return <div className="process_board"></div>;
+  return (
+    <div className="process_board">
+      <PhaseSelector></PhaseSelector>
+    </div>
+  );
 }
