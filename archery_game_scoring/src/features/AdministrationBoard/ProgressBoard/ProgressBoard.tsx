3450f2eeb60382567566d4771824bf5d53457d63
Otmeal <songoatmeal@gmail.com>
2023-10-22 21:54:12 +0800

    fix: fix end switch not showing properly

--- archery_game_scoring/src/features/AdministrationBoard/ProgressBoard/ProgressBoard.tsx
+++ archery_game_scoring/src/features/AdministrationBoard/ProgressBoard/ProgressBoard.tsx
@@ -1,11 +1,13 @@
 import { Box } from "@mui/material";
 import PhaseSelector from "../PhaseSelector/PhaseSelector";
+import EndSwitch from "./EndSwitch/EndSwitch";
 
 export default function ProgressBoard() {
   return (
     <div className="process_board">
-      <Box sx={{ width: "200px", height: "100%" }}>
-        <PhaseSelector></PhaseSelector>
+      <PhaseSelector></PhaseSelector>
+      <Box sx={{ width: "300px", height: "100%" }}>
+        <EndSwitch></EndSwitch>
       </Box>
     </div>
   );
