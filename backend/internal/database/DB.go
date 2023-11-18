939f97caa00fbc2cc8b60d3690ec22fe75399664
JasonTuTW <74854011+JasonTuTW@users.noreply.github.com>
2023-11-18 11:47:35 +0800

    feat: sleep 3 s for each retry, exit(1) finally (#49)

--- backend/internal/database/DB.go
+++ backend/internal/database/DB.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"log"
+	"time"
 	"gopkg.in/yaml.v2"
 
 	"gorm.io/driver/mysql"
@@ -51,8 +52,16 @@ func connectDB() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local&tls=skip-verify",
 		DSN.Username, DSN.Password, DSN.Host, DSN.Port, DSN.Database)
 	var err error
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	for retry := 0; retry < 5; retry++ {
+		DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err != nil {
+			fmt.Println("database connection error: ", err)
+		}
+		time.Sleep(3 * time.Second)
+	}
 	if err != nil {
-		fmt.Println("資料庫徹底連線失敗：", err)
+		fmt.Println("failed to connect database")
+		os.Exit(1)
 	}
 }
