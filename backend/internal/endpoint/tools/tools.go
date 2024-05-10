02db554231695c3b89c39475dacc6b2a29b85f73
Mushroom-MSL1L <mushroom.ac.cs11@nycu.edu.tw>
2024-05-10 14:52:01 +0800

    test : tools unit test coverage 100%
    1. tools unit test coverage 100%
    2. Convert2int
    3. Convert2bool
    4. Convert2uint
    5. Scorefmt
    6. and move the files to a sub packet file

--- backend/internal/endpoint/tools/tools.go
+++ backend/internal/endpoint/tools/tools.go
@@ -1,4 +1,4 @@
-package endpoint
+package tools
 
 import (
 	"strconv"
---
+++ backend/internal/endpoint/tools/tools_test.go
@@ -0,0 +1,111 @@
+package tools
+
+import (
+	"fmt"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestConvert2int(t *testing.T) {
+	testCases := []struct {
+		name     string
+		value    string
+		expected int
+	}{
+		{"valid int string", "-17", -17},
+		{"valid int string", "0", 0},
+		{"valid int string", "1", 1},
+		{"valid int string", "123", 123},
+		{"valid int string", "2341234123", 2341234123},
+	}
+
+	Convey("Given a valid string representation of an integer", t, func() {
+		for _, tc := range testCases {
+			error_response := fmt.Sprintf("The result should be the integer value of the string, but got %s.", tc.value)
+			c, _ := gin.CreateTestContext(httptest.NewRecorder())
+			c.Params = append(c.Params, gin.Param{Key: "int", Value: tc.value})
+			result := Convert2int(c, "int")
+			Convey(error_response, func() {
+				So(result, ShouldEqual, tc.expected)
+			})
+		}
+	})
+}
+
+func TestConvert2Bool(t *testing.T) {
+	testCases := []struct {
+		name     string
+		value    string
+		expected bool
+	}{
+		{"valid bool string", "true", true},
+		{"valid bool string", "false", false},
+		{"valid bool string", "1", true},
+		{"valid bool string", "0", false},
+	}
+
+	Convey("Given a valid string representation of an bool", t, func() {
+		for _, tc := range testCases {
+			error_response := fmt.Sprintf("The result should be the boolean value of the string, got %s.", tc.value)
+			c, _ := gin.CreateTestContext(httptest.NewRecorder())
+			c.Params = append(c.Params, gin.Param{Key: "bool", Value: tc.value})
+			result := Convert2bool(c, "bool")
+			Convey(error_response, func() {
+				So(result, ShouldEqual, tc.expected)
+			})
+		}
+	})
+}
+
+func TestConvert2uint(t *testing.T) {
+	testCases := []struct {
+		name     string
+		value    string
+		expected uint
+	}{
+		{"valid int string", "-17", 0},
+		{"valid int string", "0", 0},
+		{"valid int string", "1", 1},
+		{"valid int string", "123", 123},
+		{"valid int string", "2341234123", 2341234123},
+	}
+
+	Convey("Given a valid string representation of an uint", t, func() {
+		for _, tc := range testCases {
+			error_response := fmt.Sprintf("The result should be the unsigned int value of the string, got %s.", tc.value)
+			c, _ := gin.CreateTestContext(httptest.NewRecorder())
+			c.Params = append(c.Params, gin.Param{Key: "uint", Value: tc.value})
+			result := Convert2uint(c, "uint")
+			Convey(error_response, func() {
+				So(result, ShouldEqual, tc.expected)
+			})
+		}
+	})
+}
+
+func TestScorefmt(t *testing.T) {
+	testcase := []struct {
+		name     string
+		score    int
+		expected int
+	}{
+		{"score less than 0", -1, 0},
+		{"score equal to 0", 0, 0},
+		{"score between 0 and 10", 5, 5},
+		{"score equal to 10", 10, 10},
+		{"score greater than 10", 11, 10},
+	}
+
+	Convey("Given a score", t, func() {
+		for _, tc := range testcase {
+			error_response := fmt.Sprintf("The result should be the score value %s, got %d.", tc.name, tc.score)
+			result := Scorefmt(tc.score)
+			Convey(error_response, func() {
+				So(result, ShouldEqual, tc.expected)
+			})
+		}
+	})
+}
