ec52dcb32ec4ab50d59b86bd2d7df7cb42af26d7
Otmeal <songoatmeal@gmail.com>
2024-09-15 09:05:21 +0800

    feat: competition/admin default page layout

--- frontend/src/app/competition/[id]/admin/layout.tsx
+++ frontend/src/app/competition/[id]/admin/layout.tsx
@@ -1,50 +1,107 @@
 "use client";
-import { TabList, TabPanel, TabContext } from "@mui/lab";
-import { Tab } from "@mui/material";
+import prunePath from "@/utils/prunePath";
+import { Tab, Tabs } from "@mui/material";
+import { usePathname, useRouter } from "next/navigation";
 
 import { useState } from "react";
 
-export default function Layout({
-  main,
-  participants,
-  groups,
-  schedule,
-  progress,
-  score_edition,
-}: {
-  main: JSX.Element;
-  participants: JSX.Element;
-  groups: JSX.Element;
-  schedule: JSX.Element;
-  progress: JSX.Element;
-  score_edition: JSX.Element;
-}) {
-  const [tabValue, setTabValue] = useState("1");
-
-  const handleTabChange = (_event: React.ChangeEvent<{}>, newValue: string) => {
-    setTabValue(newValue);
-  };
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const [tabValue, setTabValue] = useState(0);
+  const currentPath = prunePath(usePathname(), 4);
 
   return (
-    <TabContext value={tabValue}>
-      <TabList
-        onChange={handleTabChange}
+    <>
+      <Tabs
         centered
+        value={tabValue}
         aria-label="Administration board"
+        role="navigation"
       >
-        <Tab label="主面板" value={"1"} />
-        <Tab label="參加者管理" value={"2"} />
-        <Tab label="組別管理" value={"3"} />
-        <Tab label="賽程" value={"4"} />
-        <Tab label="進度" value={"5"} />
-        <Tab label="分數編輯" value={"6"} />
-      </TabList>
-      <TabPanel value="1">{main}</TabPanel>
-      <TabPanel value="2">{participants}</TabPanel>
-      <TabPanel value="3">{groups}</TabPanel>
-      <TabPanel value="4">{schedule}</TabPanel>
-      <TabPanel value="5">{progress}</TabPanel>
-      <TabPanel value="6">{score_edition}</TabPanel>
-    </TabContext>
+        <LinkTab
+          href="/main"
+          label="主面板"
+          selected={tabValue === 0}
+          currentPath={currentPath}
+          setTabValue={setTabValue}
+          value={0}
+        />
+        <LinkTab
+          href="/participants"
+          label="參加者管理"
+          selected={tabValue === 1}
+          currentPath={currentPath}
+          setTabValue={setTabValue}
+          value={1}
+        />
+        <LinkTab
+          href="/groups"
+          label="組別管理"
+          selected={tabValue === 2}
+          currentPath={currentPath}
+          setTabValue={setTabValue}
+          value={2}
+        />
+        <LinkTab
+          href="/schedule"
+          label="賽程"
+          selected={tabValue === 3}
+          currentPath={currentPath}
+          setTabValue={setTabValue}
+          value={3}
+        />
+        <LinkTab
+          href="/progress"
+          label="進度"
+          selected={tabValue === 4}
+          currentPath={currentPath}
+          setTabValue={setTabValue}
+          value={4}
+        />
+        <LinkTab
+          href="/score_edition"
+          label="分數編輯"
+          selected={tabValue === 5}
+          currentPath={currentPath}
+          setTabValue={setTabValue}
+          value={5}
+        />
+      </Tabs>
+      {children}
+    </>
+  );
+}
+interface LinkTabProps {
+  currentPath: string;
+  label: string;
+  href: string;
+  selected: boolean;
+  setTabValue: (value: number) => void;
+  value: number;
+}
+
+function LinkTab({
+  currentPath,
+  label,
+  href,
+  selected,
+  setTabValue,
+  value,
+}: LinkTabProps) {
+  const router = useRouter();
+  const props = {
+    label,
+
+    selected,
+  };
+  return (
+    <Tab
+      component="a"
+      onClick={(_event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        router.push(currentPath + href);
+        setTabValue(value);
+      }}
+      aria-current={selected && "page"}
+      {...props}
+    />
   );
 }
