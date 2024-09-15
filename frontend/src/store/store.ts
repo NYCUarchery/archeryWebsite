756627f93ceac5090662f0fc9c33d36548812653
Otmeal <songoatmeal@gmail.com>
2024-09-15 09:23:07 +0800

    feat: set up store for TS

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,10 +1,17 @@
 import { Middleware, configureStore } from "@reduxjs/toolkit";
 import ReduxLogger from "redux-logger";
+import qualificationScheduleSlice from "app/competition/[id]/admin/schedule/qualification/qualificationScheduleSlice";
+import scheduleSlice from "app/competition/[id]/admin/schedule/scheduleSlice";
 
 const store = configureStore({
-  reducer: {},
-  middleware: (getDefaultMiddleware) =>
+  reducer: {
+    qualificationSchedule: qualificationScheduleSlice,
+    schedule: scheduleSlice,
+  },
+  middleware: (getDefaultMiddleware: any) =>
     getDefaultMiddleware().concat(ReduxLogger as Middleware),
 });
 
 export default store;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
