0ffa63804369b1569091a0aedf85d4d2600cd9db
Otmeal <songoatmeal@gmail.com>
2024-11-02 09:54:04 +0800

    fix: refetching user data when not logged in

--- frontend/src/app/(home)/layout.tsx
+++ frontend/src/app/(home)/layout.tsx
@@ -3,6 +3,7 @@ import Header from "./Header";
 import { useState } from "react";
 import Sidebar from "./Sidebar";
 import { Box } from "@mui/material";
+import { useGetCurrentUserDetail } from "@/utils/QueryHooks/useGetCurrentUserDetail";
 
 export default function RootLayout({
   children,
@@ -10,10 +11,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const { data: user, isFetched: isUserFetched } = useGetCurrentUserDetail();
 
   return (
     <>
-      <Header setSideBarOpen={setSidebarOpen} />
+      <Header
+        setSideBarOpen={setSidebarOpen}
+        user={user}
+        isUserFetched={isUserFetched}
+      />
       <Sidebar setSideBarOpen={setSidebarOpen} sideBarOpen={sidebarOpen} />
       <Box
         sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
