bdcfc46911fa740cf4bdec52b6a3ea0e60698c92
Mushroom-MSL1L <mushroom.ac.cs11@nycu.edu.tw>
2024-11-09 00:43:12 +0800

    docs : update all swag docs

--- backend/docs/docs.go
+++ backend/docs/docs.go
@@ -2698,14 +2698,14 @@ const docTemplate = `{
         },
         "/matchresult/matchend/scores/{id}": {
             "patch": {
-                "description": "Update one MatchEnd totalScores by id and all related MatchScores by MatchScore ids.\nMatchScore ids and scores must be the same length",
+                "description": "Update one MatchEnd totalScores by id and all related MatchScores by MatchScore ids\nMatchScore ids and scores must be the same length",
                 "consumes": [
                     "application/json"
                 ],
                 "tags": [
                     "MatchEnd"
                 ],
-                "summary": "Update one MatchEnd scores.",
+                "summary": "Update one MatchEnd scores",
                 "parameters": [
                     {
                         "type": "integer",
@@ -3279,7 +3279,7 @@ const docTemplate = `{
                         }
                     },
                     "400": {
-                        "description": "invalid info / role is empty / participant exists / invalid user ID / invalid competition ID",
+                        "description": "invalid info / role is not defined / participant exists / invalid user ID / invalid competition ID",
                         "schema": {
                             "$ref": "#/definitions/response.ErrorReceiveDataFormatResponse"
                         }
@@ -3381,6 +3381,53 @@ const docTemplate = `{
                     },
                     "500": {
                         "description": "internal db error / Get participants by competition id / Get user by id",
+                        "schema": {
+                            "$ref": "#/definitions/response.ErrorInternalErrorResponse"
+                        }
+                    }
+                }
+            }
+        },
+        "/participant/me/{competitionid}": {
+            "get": {
+                "description": "Get Participants By user id and competition id.\nAnd update session gamerole.\nWarnings: Something need to be modified in the future.\nWarnings: Only take the first one temporarily, for player and dummy player assumption in one competition of a user.",
+                "produces": [
+                    "application/json"
+                ],
+                "tags": [
+                    "Participant"
+                ],
+                "summary": "Get Participants By user id and competition id, and update session gamerole.",
+                "parameters": [
+                    {
+                        "type": "integer",
+                        "description": "Competition ID",
+                        "name": "competitionid",
+                        "in": "path",
+                        "required": true
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "success, return the first participant, and update session gamerole",
+                        "schema": {
+                            "$ref": "#/definitions/database.Participant"
+                        }
+                    },
+                    "400": {
+                        "description": "invalid competition id / invalid user id",
+                        "schema": {
+                            "$ref": "#/definitions/response.ErrorIdResponse"
+                        }
+                    },
+                    "401": {
+                        "description": "unauthorized",
+                        "schema": {
+                            "$ref": "#/definitions/response.ErrorUnauthorizedResponse"
+                        }
+                    },
+                    "500": {
+                        "description": "get participants by competition id and user id",
                         "schema": {
                             "$ref": "#/definitions/response.ErrorInternalErrorResponse"
                         }
@@ -5399,7 +5446,7 @@ const docTemplate = `{
         },
         "/user": {
             "post": {
-                "description": "Add a user to db.\nUsername cannot be empty or repeated.\nPassword cannot be empty.\nEmail cannot be empty or repeated.",
+                "description": "Add a user to db.\nUsername cannot be empty or repeated.\nPassword cannot be empty.\nEmail cannot be empty or repeated.\nInstitution id must exist.\nRealname and overview are optional.\nRole is set to User.",
                 "consumes": [
                     "application/json"
                 ],
@@ -6111,6 +6158,9 @@ const docTemplate = `{
                 "real_name": {
                     "type": "string"
                 },
+                "role": {
+                    "type": "string"
+                },
                 "user_name": {
                     "type": "string"
                 }
@@ -6735,6 +6785,15 @@ const docTemplate = `{
                 }
             }
         },
+        "response.ErrorUnauthorizedResponse": {
+            "type": "object",
+            "properties": {
+                "error": {
+                    "type": "string",
+                    "example": "unauthorized"
+                }
+            }
+        },
         "response.Nill": {
             "type": "object"
         },
@@ -6803,7 +6862,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:80",
+	Host:             "127.0.0.1:80",
 	BasePath:         "/api/",
 	Schemes:          []string{},
 	Title:            "Gin swagger",
